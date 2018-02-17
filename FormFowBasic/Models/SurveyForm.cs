9d8b12a3dbb04dd27a548c0461affd505aca0990
Hinault <ngdonfack@gmail.com>
2018-02-17 09:44:53 -0500

    Update SurveyForm

--- FormFowBasic/Models/SurveyForm.cs
+++ FormFowBasic/Models/SurveyForm.cs
@@ -7,16 +7,27 @@ namespace FormFowBasic.Models
     [Serializable]
     public class SurveyForm
     {
+        public JobOptions Job;
+        public ExperienceOptions Experience;
+        public PlatformOptions Platform;
+        public LanguageOptions Language;
+        public DatabaseOptions Database;
 
+        public static IForm<SurveyForm> BuildForm()
+        {
+            return new FormBuilder<SurveyForm>()
+                    .Message("Merci de prendre quelques minutes pour repondre aux questions de cette enquête.")
+                    .Build();
+        }
 
     }
 
 
-    public enum JobOptions { };
-    public enum ExperienceOptions { };
-    public enum PlatformOptions { };
-    public enum LanguageOptions { };
-    public enum DatabaseOptions { };
-    public enum WebFrameworkOptions { };
-    public enum CloudOptions { };
+    public enum JobOptions {Developpeur_junior=1, Developpeur_senior, Architecte, Autre};
+    public enum ExperienceOptions {Moin_de_5_ans=1, De_5_a_10_ans};
+    public enum PlatformOptions {Web =1, Mobile, Cloud};
+    public enum LanguageOptions {Csharp=1, Java, JavaScript, C, Ruby, Python,  Autre};
+    public enum DatabaseOptions {Sql_Server=1, MySQL, Oracle };
+   /* public enum WebFrameworkOptions { };
+    public enum CloudOptions { };*/
 }
