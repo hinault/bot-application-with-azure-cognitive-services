2313810e02d3c6e1827e9c518782fe41beb402af
Hinault <ngdonfack@gmail.com>
2018-02-17 09:12:20 -0500

    Add Form Survey

---
+++ FormFowBasic/Models/SurveyForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace FormFowBasic.Models
+{
+    [Serializable]
+    public class SurveyForm
+    {
+
+
+    }
+
+
+    public enum JobOptions { };
+    public enum ExperienceOptions { };
+    public enum PlatformOptions { };
+    public enum LanguageOptions { };
+    public enum DatabaseOptions { };
+    public enum WebFrameworkOptions { };
+    public enum CloudOptions { };
+}
