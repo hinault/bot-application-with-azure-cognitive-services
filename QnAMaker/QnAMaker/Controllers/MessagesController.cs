bda530a77e759ed64388afb3936db03fd4f0e971
Hinault <ngdonfack@gmail.com>
2018-03-06 21:53:54 -0500

    Update messagecontroller

--- QnAMaker/QnAMaker/Controllers/MessagesController.cs
+++ QnAMaker/QnAMaker/Controllers/MessagesController.cs
@@ -18,7 +18,7 @@ namespace QnAMaker
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.QnaDialog());
             }
             else
             {
