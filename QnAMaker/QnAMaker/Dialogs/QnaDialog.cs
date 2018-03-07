9187d1e9d57b43592902ee36d2a4ac0829935afe
Hinault <ngdonfack@gmail.com>
2018-03-06 21:52:27 -0500

    Update QnaDialog

--- QnAMaker/QnAMaker/Dialogs/QnaDialog.cs
+++ QnAMaker/QnAMaker/Dialogs/QnaDialog.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System.Configuration;
 
 namespace QnAMaker.Dialogs
 {
-    public class QnaDialog
+    [Serializable]
+    public class QnaDialog : QnAMakerDialog
     {
+        public QnaDialog() : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnaSubscriptionKey"],
+            ConfigurationManager.AppSettings["QnaKnowledgebaseId"], "Désolé, je n'ai pas trouvé une reponse à cette question",0.5)))
+        { }
     }
 }
