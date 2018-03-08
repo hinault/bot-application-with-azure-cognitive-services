8cb85ab18561910a9a274e839a4e37d0717181f8
Hinault <ngdonfack@gmail.com>
2018-03-07 19:22:31 -0500

    Update QnaDialog

--- QnAMaker/QnAMaker/Dialogs/QnaDialog.cs
+++ QnAMaker/QnAMaker/Dialogs/QnaDialog.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
 using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace QnAMaker.Dialogs
 {
@@ -8,7 +14,27 @@ namespace QnAMaker.Dialogs
     public class QnaDialog : QnAMakerDialog
     {
         public QnaDialog() : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnaSubscriptionKey"],
-            ConfigurationManager.AppSettings["QnaKnowledgebaseId"], "Désolé, je n'ai pas trouvé une reponse à cette question",0.5)))
+            ConfigurationManager.AppSettings["QnaKnowledgebaseId"], "Désolé, je n'ai pas trouvé une reponse à cette question", 0.5)))
         { }
+
+        protected override async Task RespondFromQnAMakerResultAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
+        {
+            // answer is a string
+            var answer = result.Answers.First().Answer;
+
+         
+
+            Activity reply = ((Activity)context.Activity).CreateReply();
+
+            try { 
+            var response = JObject.Parse(answer);
+            }
+            catch(JsonReaderException)
+            { 
+            reply.Text = answer;
+            }
+            await context.PostAsync(reply);
+
+        }
     }
 }
