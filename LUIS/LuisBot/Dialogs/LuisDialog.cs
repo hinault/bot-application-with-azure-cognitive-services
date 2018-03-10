0d05ed8e3126f0153698c0da054b20442c4d6c76
Hinault <ngdonfack@gmail.com>
2018-03-09 20:54:33 -0500

    Update Web.config

--- LUIS/LuisBot/Dialogs/LuisDialog.cs
+++ LUIS/LuisBot/Dialogs/LuisDialog.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System.Configuration;
 
 namespace LuisBot.Dialogs
 {
-    public class LuisDialog
+    [Serializable]
+    public class LuisDialog : LuisDialog<object>
     {
+        public LuisDialog() : base(new LuisService(new LuisModelAttribute(
+            ConfigurationManager.AppSettings["LuisAppId"],
+            ConfigurationManager.AppSettings["LuisAPIKey"],
+            domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
+        {
+        }
+      
+   
     }
 }
