476c581d0e934a897c8efc7de123c86784a7a697
Hinault <ngdonfack@gmail.com>
2018-03-17 11:49:56 -0400

    Update service

--- Text-Analytics-API/SentimentAnalysisBot/Models/TextAnalyticsResults.cs
+++ Text-Analytics-API/SentimentAnalysisBot/Models/TextAnalyticsResults.cs
@@ -7,5 +7,14 @@ namespace SentimentAnalysisBot.Models
 {
     public class TextAnalyticsResults
     {
+
+        public double? Score {get; set;}
+
+        public string LanguageName { get; set; }
+
+        public string LanguageIso6391Name { get; set; }
+
+        public IList<string> KeyPhrases { get; set; }
+
     }
 }
---
+++ Text-Analytics-API/SentimentAnalysisBot/Services/TextAnalyticsService.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System.Configuration;
+using System.Collections.Generic;
+using SentimentAnalysisBot.Models;
+
+namespace SentimentAnalysisBot.Services
+{
+    public class TextAnalyticsService
+    {
+
+        public static async Task<TextAnalyticsResults> AnalyseSentiment(string input)
+        {
+
+            var client = new TextAnalyticsAPI
+            {
+                AzureRegion = AzureRegions.Westus,
+                SubscriptionKey = ConfigurationManager.AppSettings["TextAnalyticsSubscriptionKey"]
+            };
+
+
+        
+            var language = client.DetectLanguage(
+                   new BatchInput(
+                       new List<Input>()
+                       {
+                          new Input("1", input)
+                       }));
+
+            var result = new TextAnalyticsResults
+            {
+                 LanguageName = language.Documents[0].DetectedLanguages[0].Name,
+                 LanguageIso6391Name = language.Documents[0].DetectedLanguages[0].Iso6391Name
+            };
+
+
+            var keyPhrases = await client.KeyPhrasesAsync(new MultiLanguageBatchInput(
+                  new[] { new MultiLanguageInput(language:result.LanguageIso6391Name, id: "1", text: input) }));
+
+            result.KeyPhrases = keyPhrases.Documents[0].KeyPhrases;
+
+            var sentiment = await client.SentimentAsync(new MultiLanguageBatchInput(
+                   new[] { new MultiLanguageInput(language:result.LanguageIso6391Name, id:"1", text: input) }));
+
+            result.Score = sentiment.Documents[0].Score;
+            
+
+
+            return result;
+        }
+    }
+}
