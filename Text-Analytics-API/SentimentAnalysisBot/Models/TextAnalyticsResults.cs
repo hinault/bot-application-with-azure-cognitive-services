1df711dbf773ec7f325f9616652775b329a3cb7c
Hinault <ngdonfack@gmail.com>
2018-03-17 11:22:55 -0400

    Add models

---
+++ Text-Analytics-API/SentimentAnalysisBot/Models/TextAnalyticsResults.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SentimentAnalysisBot.Models
+{
+    public class TextAnalyticsResults
+    {
+    }
+}
