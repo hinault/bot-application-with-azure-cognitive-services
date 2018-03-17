411f6a5ef1fa5b452835b65c7da717a7316ffaa8
Hinault <ngdonfack@gmail.com>
2018-03-17 10:00:31 -0400

    update service

--- Text-Analytics-API/SentimentAnalysisBot/Services/SentimentAnalysisService.cs
+++ Text-Analytics-API/SentimentAnalysisBot/Services/SentimentAnalysisService.cs
@@ -1,18 +1,27 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System.Configuration;
 
 namespace SentimentAnalysisBot.Services
 {
     public class SentimentAnalysisService
     {
 
-        public static async Task<float> AnalyseSentiment(string message)
+        public static async Task<double> AnalyseSentiment(string message)
         {
 
-            ITextAnalyticsAPI client = new TextAnalyticsAPI();
+            var client = new TextAnalyticsAPI
+            {
+                AzureRegion = AzureRegions.Westus,
+                SubscriptionKey = ConfigurationManager.AppSettings["TextAnalyticsSubscriptionKey"]
+            };
 
 
+            var sentiment = await client.SentimentAsync(new MultiLanguageBatchInput(
+                   new[] { new MultiLanguageInput("fr", text: message) }));
 
+            return sentiment.Documents[0].Score.Value;
         }
     }
 }
